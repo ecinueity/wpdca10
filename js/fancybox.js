fc5725cae21665301b5d57789bc70a6fc6ef52b9
ecinueity <eunicenice_@outlook.com>
2017-02-18 21:08:53 +0800

    Fancybox

---
+++ js/fancybox.js
@@ -0,0 +1,27 @@
+<!-- For typical fancybox -->
+<script type="text/javascript">
+	$(document).ready(function() {
+		$(".fancybox").fancybox();
+	});
+</script>
+
+<!-- For image gallery using fancybox -->
+<script type="text/javascript>
+$(document).ready(function() {
+	$(".fancybox-thumb").fancybox({
+		prevEffect	: 'none',
+		nextEffect	: 'none',
+		helpers	: {
+			title	: {
+				type: 'outside'
+			},
+			thumbs	: {
+				width	: 50,
+				height	: 50
+			}
+		}
+	});
+});
+</script>
+
+
